5ca899e970eb91bfc79645c6c6be1fedb7d1e5be
cliff <cliff@6fdb692e-5d52-e011-adc2-000e0c3ec3fd>
2011-03-20 20:36:54 +0000

    Список аусвайсов и команд - каркас
    git-svn-id: svn://dev.cliffa.net/ausweis@78 6fdb692e-5d52-e011-adc2-000e0c3ec3fd

--- lib/DB/Blok.pm
+++ lib/DB/Blok.pm
@@ -1,12 +1,12 @@
-package DB::Command;
+package DB::Blok;
 
 use strict;
 use warnings;
 
 use base 'Clib::DBIC';
 
-__PACKAGE__->table("command");
-__PACKAGE__->columns_array(qw/id dtadd blkid name/);
+__PACKAGE__->table("blok");
+__PACKAGE__->columns_array(qw/id dtadd name/);
 
 sub create {
     my ($self, $new) = @_;
