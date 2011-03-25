e9ed9273678a2133c50687581399d7725db08364
cliff <cliff@6fdb692e-5d52-e011-adc2-000e0c3ec3fd>
2011-03-25 20:18:09 +0000

    Доп поля
    git-svn-id: svn://dev.cliffa.net/ausweis@128 6fdb692e-5d52-e011-adc2-000e0c3ec3fd

--- lib/DB/Ausweis.pm
+++ lib/DB/Ausweis.pm
@@ -6,7 +6,8 @@ use warnings;
 use base 'Clib::DBIC';
 
 __PACKAGE__->table("ausweis");
-__PACKAGE__->columns_array(qw/id dtadd numid cmdid blkid nick/);
+__PACKAGE__->columns_array(qw/id dtadd numid cmdid blkid blocked nick
+                                 fio krov allerg neperenos polis medik comment/);
 
 __PACKAGE__->link(command => 'Command', cmdid => 'id', {join_type => 'left'});
 __PACKAGE__->link(blok => 'Blok', blkid => 'id', {join_type => 'left'});
