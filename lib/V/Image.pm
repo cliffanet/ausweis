1bd681759410a1f75afe9d7f8e099f67f252df6f
cliff <cliff@6fdb692e-5d52-e011-adc2-000e0c3ec3fd>
2011-03-26 16:48:34 +0000

    Изображение для печати - каркас - отладка
    git-svn-id: svn://dev.cliffa.net/ausweis@203 6fdb692e-5d52-e011-adc2-000e0c3ec3fd

--- lib/V/Image.pm
+++ lib/V/Image.pm
@@ -46,7 +46,7 @@ sub render {
         if (!open(IMG, '>:raw', \$out)) {
             $self->r->error("Can't open img-handler");
         } 
-        elsif (my $error = $d->{img}->Write('png:-', file => \*IMG)) {
+        elsif (my $error = $d->{img}->Write('png:$out')) {
             $self->r->error("Write PNG ERROR: $error");
         }
         else {
