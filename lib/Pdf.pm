4a9e22fa8941cc4ac3cc92cd76f440ed167ee56d
cliff <cliff@6fdb692e-5d52-e011-adc2-000e0c3ec3fd>
2011-07-10 22:15:13 +0000

    Печать в PDF
    git-svn-id: svn://dev.cliffa.net/ausweis@386 6fdb692e-5d52-e011-adc2-000e0c3ec3fd

--- lib/Pdf.pm
+++ lib/Pdf.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use PDF::API2;
 use Clib::Mould;
+use Image::Magick;
 
 ##################################################################
 ######
@@ -39,13 +40,13 @@ sub Ausweis {
             $self->debug("opts[$p]: if=$if");
             my $ret = eval $if;
             $self->error("opts[$p]: if=$if; ERROR: $@") if $@;
-            $ret || next;
+            $ret || return;
         }
         
         if (lc($p) eq 'offset') {
             $ox = $o->{x} if defined $o->{x};
             $oy = $o->{y} if defined $o->{y};
-            next;
+            return;
         }
         
         $o->{$_} += $ox
@@ -53,14 +54,14 @@ sub Ausweis {
         $o->{$_} += $oy
             foreach grep { defined $o->{$_} } qw/y y1 y2/;
         $o->{$_} *= 2.835
-            foreach grep { defined $o->{$_} } qw/x x1 x2 y y1 y2/;
+            foreach grep { defined $o->{$_} } qw/x x1 x2 y y1 y2 width height/;
         $o->{$_} = 842-$o->{$_}
             foreach grep { defined $o->{$_} } qw/y y1 y2/;
         
         if (lc($p) eq 'area') {
             $self->debug('AREA: %s', join('', Dumper($o)));
             my $gfx = $page->gfx;
-            $gfx->strokecolor($o->{color}) if $o->{color};
+            $gfx->strokecolor($o->{color}||'black');
             $gfx->move($o->{x1}||0, $o->{y1}||0);
             $gfx->line($o->{x2}||0, $o->{y1}||0);
             $gfx->line($o->{x2}||0, $o->{y2}||0);
@@ -79,10 +80,10 @@ sub Ausweis {
             $txt->textstart;
             $txt->font($fnt, $o->{size}||10);
             $txt->translate($o->{x}||0, $o->{y}||0);
-            $txt->fillcolor($o->{color}) if $o->{color};
-            if ($o->{align} eq 'right') {
+            $txt->fillcolor($o->{color}||'black');
+            if ($o->{align} && ($o->{align} eq 'right')) {
                 $txt->text_right($o->{text});
-            } elsif ($o->{align} eq 'center') {
+            } elsif ($o->{align} && ($o->{align} eq 'center')) {
                 $txt->text_center($o->{text});
             } else {
                 $txt->text($o->{text});
@@ -95,12 +96,30 @@ sub Ausweis {
             my $file = lc($p) eq 'photo' ? 
                 Func::UserDir($rec->{id})."/photo.aus.jpg" :
                 "$::dirPhoto/command/$rec->{command}->{photo}";
+                
+            my ($w, $h);
+            {
+                my $img1 = Image::Magick->new();
+                $img1->Read($file);
+                $img1->AutoOrient();
+                ($w, $h) = ($img1->Get('width'), $img1->Get('height'));
+                my $k = $o->{width} && ($o->{width} < $w) ? $o->{width}/$w : 1;
+                $k = $o->{height}/$h if $o->{height} && (($o->{height}/$h) < $k);
+                $self->debug("IMG: orig = %dx%d, k=%0.4f, new = %dx%d", $w, $h, $k, $w*$k, $h*$k);
+                ($w, $h) = ($w*$k, $h*$k);
+                if ($o->{width} && $o->{align} && ($o->{align} =~ /right/i)) {
+                    $o->{x} += $o->{width}-$w if $o->{width}>$w;
+                } elsif ($o->{width} && $o->{align} && ($o->{align} =~ /center/i)) {
+                    $o->{x} += ($o->{width}-$w)/2 if $o->{width}>$w;
+                }
+            }
             
             my $gfx = $page->gfx;
             my $img = $pdf->image_jpeg($file);
-            $gfx->image( $img, $o->{x}, $o->{y} );#, $o->{x}, $o->{y} );
+            $gfx->image( $img, $o->{x}, $o->{y}, $w, $h );
         }
-#        elsif ((lc($p) eq 'barcode') && $rec->{numid}) {
+        elsif ((lc($p) eq 'barcode') && $rec->{numid}) {
+            $self->debug('BarCode: %s', join('', Dumper($o)));
 #            my $file = "$::dirPhoto/barcode/$rec->{numid}.jpg";
 #            if (-f $file) { {
 #                my $img1 = Image::Magick->new();
@@ -114,7 +133,23 @@ sub Ausweis {
 #                $error = $img->Composite(image => $img1, x=>$o->{x}, y=>$o->{y});
 #                $error && last;
 #            } }
-#        }
+            my $gfx = $page->gfx;
+            my $fnt = $pdf->ttfont("$::font_dir/arial.ttf", -encode=>'cp1251'); 
+            my $bc =  $pdf->xo_code128(
+                    -font   => $fnt,    # the font to use for text
+                    -type   => 'code128a',    # the type of barcode
+                    -code   => $rec->{numid}, # the code of the barcode
+                #    -extn   => '012345',    # the extension of the barcode
+                    -umzn   => 10,         # (u)pper (m)ending (z)o(n)e -  высота штриха
+                #    -lmzn   => 10,         # (l)ower (m)ending (z)o(n)e -  высота теста, если нужен текст
+                    -zone   => $o->{height},         # height (zone) of bars,
+                #    -quzn   => 10,         # (qu)iet (z)o(n)e - горизонтальные поля
+                #    -ofwt   => 0.01,       # (o)ver(f)low (w)id(t)h
+                    #-fnsz   => 5,          # (f)o(n)t(s)i(z)e
+                    #-text   => 'alternative text'
+                    );
+            my $fi = $gfx->formimage($bc, $o->{x}, $o->{y});
+        }
         
         };
         
