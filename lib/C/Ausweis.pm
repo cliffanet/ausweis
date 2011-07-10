35de8d69fb1e64760de2234d35fe2024658abbba
cliff <cliff@6fdb692e-5d52-e011-adc2-000e0c3ec3fd>
2011-07-10 11:06:00 +0000

    отладка фото
    git-svn-id: svn://dev.cliffa.net/ausweis@369 6fdb692e-5d52-e011-adc2-000e0c3ec3fd

--- lib/C/Ausweis.pm
+++ lib/C/Ausweis.pm
@@ -143,7 +143,7 @@ sub img {
 
     return unless $self->rights_exists_event($::rAusweisInfo);
     
-    $type = 'info' if !$type || ($type !~ /^(front|rear)$/);
+    $type = 'front' if !$type || ($type !~ /^(front|rear)$/);
     
     my ($rec) = (($self->d->{rec}) = 
         #map { _item($self, $_) }
@@ -154,99 +154,10 @@ sub img {
         return unless $self->rights_check_event($::rAusweisInfo, $::rAll);
     }
     
-    my $width = $::print{width} || 200;
-    my $height= $::print{height}|| 400;
-    my $img = ($self->d->{img} = Image::Magick->new(size => "${width}x${height}"));
-    $img || return $self->state(-000100, '');
-    my $bg = $::print{bgcolor} || 'transparent';
-    $img->ReadImage("xc:$bg");
-    if ($::print{density}) {
-        $img->Set(density=>$::print{density});
-        $img->Set(units=>"PixelsPerInch");
-    }
-    
+    $self->d->{img} = Func::ausweis_img($self, $rec, $type)
+        || return $self->state(-000100, '');
     
     $self->view_select('Image');
-
-    my @opt = @{ $::print{$type} || [] };
-    use Data::Dumper;
-    while (my $p = shift @opt) {
-        my $o = shift @opt || next;
-        next unless ref($o) eq 'HASH';
-        
-        #$self->debug("opts[$p]: ".Dumper($o));
-        if (my $if = delete $o->{if}) {
-            $self->debug("opts[$p]: if=$if");
-            my $ret = eval $if;
-            $self->error("opts[$p]: if=$if; ERROR: $@") if $@;
-            $ret || next;
-        }
-        
-        my $error;
-        if (lc($p) eq 'area') {
-            #$o->{stroke} ||= $o->{color} if $o->{color};
-            $o->{fill} ||= $bg;
-            $error = $img->Draw(primitive=>'rectangle', %$o);
-        }
-        elsif ((lc($p) eq 'text') && $o->{text} && $o->{x} && $o->{y}) {
-            my $m = Clib::Mould->new();
-            $o->{text} = $m->Parse(data => $o->{text}, pattlist => $rec, dot2hash => 1);
-            $self->debug("TEXT: $o->{text}");
-            $o->{text} = decode('cp1251', $o->{text});
-            
-            $error = $img->Annotate(antialias=>'true', %$o);
-            
-        }
-        elsif ((((lc($p) eq 'photo') && $rec->{photo}) || 
-                ((lc($p) eq 'logo') && $rec->{command}->{photo})) && 
-                    $o->{x} && $o->{y}) {
-            my $file = lc($p) eq 'photo' ? 
-                #"$::dirPhoto/ausweis/$rec->{photo}" :
-                Func::UserDir($rec->{id})."/photo.aus.jpg" :
-                "$::dirPhoto/command/$rec->{command}->{photo}";
-            {
-                my $img1 = Image::Magick->new();
-                $error = $img1->Read($file);
-                $error && last;
-                $error = $img1->AutoOrient();
-                $error && last;
-                my ($w, $h) = ($img1->Get('width'), $img1->Get('height'));
-                my $k = $o->{width} && ($o->{width} < $w) ? $o->{width}/$w : 1;
-                $k = $o->{height}/$h if $o->{height} && (($o->{height}/$h) < $k);
-                if ($k < 1) {
-                    $self->debug("IMG: orig = %dx%d, k=%0.4f, new = %dx%d", $w, $h, $k, $w*$k, $h*$k);
-                    $error = $img1->Resize(width=>int($w*$k), height=>int($h*$k));
-                    $error && last;
-                    $w = int($w*$k);
-                }
-                if ($o->{width} && $o->{align} && ($o->{align} =~ /right/i)) {
-                    $o->{x} += $o->{width}-$w if $o->{width}>$w;
-                } elsif ($o->{width} && $o->{align} && ($o->{align} =~ /center/i)) {
-                    $o->{x} += int ($o->{width}-$w)/2 if $o->{width}>$w;
-                }
-                $error = $img->Composite(image => $img1, x=>$o->{x}, y=>$o->{y});
-                $error && last;
-            }
-        }
-        elsif ((lc($p) eq 'barcode') && $rec->{numid}) {
-            my $file = "$::dirPhoto/barcode/$rec->{numid}.jpg";
-            if (-f $file) { {
-                my $img1 = Image::Magick->new();
-                $error = $img1->Read($file);
-                $error && last;
-                my ($w, $h) = ($img1->Get('width'), $img1->Get('height'));
-                $error = $img1->Crop(x=>0,y=>0, width=>$w, height=>int($h*0.4));
-                $error && last;
-                $error = $img1->Resize(width=>int($o->{width}||$w), height=>int($o->{height}||$h));
-                $error && last;
-                $error = $img->Composite(image => $img1, x=>$o->{x}, y=>$o->{y});
-                $error && last;
-            } }
-        }
-        
-        $self->error("Image::Magick ERROR(%s): %s", $p, $error)
-            if $error;
-    }
 }
 
 
